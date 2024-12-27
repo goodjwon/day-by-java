87c878b6ddcd0136e26719a0d6cd3585699f087f
skfkfkvlrm <skmfmfvlrm@gmail.com>
2024-12-27 20:40:08 +0900

    12/27

--- src/com/example/ch4/hw/txt/MergeTxt.java
+++ src/com/example/ch4/hw/txt/MergeTxt.java
@@ -7,21 +7,43 @@ public class MergeTxt {
     public static void main(String[] args) {
         String file1 = "file1.txt";
         String file2 = "file2.txt";
+        String outputFile = "file.txt";
 
-        try (PrintWriter pw = new PrintWriter(file1)) {
-            pw.println("Hello from file1");
+        BufferedReader br1 = null;
+        BufferedReader br2 = null;
+        BufferedWriter bw = null;
+
+        try {
+            br1 = new BufferedReader(new FileReader(file1));
+            br2 = new BufferedReader(new FileReader(file2));
+            bw = new BufferedWriter(new FileWriter(outputFile));
+
+            String line;
+            while ((line = br1.readLine()) != null) {
+                bw.write(line);
+                bw.newLine();
+            }
+
+            while ((line = br2.readLine()) != null) {
+                bw.write(line);
+                bw.newLine();
+            }
         } catch (IOException e) {
             e.printStackTrace();
-        }
-
-        try (PrintWriter pw = new PrintWriter(file2)){
-            pw.println("Hello from file2");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        try (){
-
+        } finally {
+            try {
+                if (br1 != null) {
+                    br1.close();
+                }
+                if (br2 != null) {
+                    br2.close();
+                }
+                if (bw != null) {
+                    bw.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
